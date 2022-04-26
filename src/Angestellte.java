d11d7745a7a569ff4ce54df6f0fb46b129fb3446
Kevin <s76004@bht-berlin.de>
2022-04-26 20:04:18 +0200

    toString()

--- src/Angestellte.java
+++ src/Angestellte.java
@@ -1,62 +1,79 @@
 
 public class Angestellte extends Person {
 
-    // Instanzvariable gehalt
-    private float gehalt;
+	// Instanzvariable gehalt
+	private float gehalt;
 
-    /**
-     * Parameterloser Konstruktor.
-     */
-    public Angestellte() {
-        super(); // macht deutlich, dass der Konstruktor der Oberklasse
-                 // aufgerufen wird
-    }
+	/**
+	 * Parameterloser Konstruktor.
+	 */
+	public Angestellte() {
+		super(); // macht deutlich, dass der Konstruktor der Oberklasse
+		// aufgerufen wird
+	}
 
-    /**
-     * Mit diesem Konstruktor erfolgt die Initialisierung der Attribute durch die
-     * uebergebenen Parameter.
-     * 
-     * @param nachname   Nachname des Angestellten
-     * @param vorname    Vorname des Angestellten
-     * @param strasse    Strasse zur Adresse des Angestellten
-     * @param hausnummer Hausnummer zur Adresse des Angestellten
-     * @param ort        Ort zur Adresse des Angestellten
-     * @param plz        PLZ zur Adresse des Angestellten
-     * @param weiblich   Flag ob Angestellter weiblich ist
-     * @param gehalt     Gehalt des Angestellten
-     */
-    public Angestellte(String nachname, String vorname, String strasse, String hausnummer, String ort, String plz,
-            boolean weiblich, float gehalt) {
-        // Aufruf des Konstruktors der Oberklasse
-        super(nachname, vorname, strasse, hausnummer, ort, plz, weiblich);
-        // Gehalt initialisieren
-        this.gehalt = gehalt;
-    }
+	/**
+	 * Mit diesem Konstruktor erfolgt die Initialisierung der Attribute durch die
+	 * uebergebenen Parameter.
+	 * 
+	 * @param nachname   Nachname des Angestellten
+	 * @param vorname    Vorname des Angestellten
+	 * @param strasse    Strasse zur Adresse des Angestellten
+	 * @param hausnummer Hausnummer zur Adresse des Angestellten
+	 * @param ort        Ort zur Adresse des Angestellten
+	 * @param plz        PLZ zur Adresse des Angestellten
+	 * @param weiblich   Flag ob Angestellter weiblich ist
+	 * @param gehalt     Gehalt des Angestellten
+	 */
+	public Angestellte(String nachname, String vorname, String strasse, String hausnummer, String ort, String plz,
+			boolean weiblich, float gehalt) {
+		// Aufruf des Konstruktors der Oberklasse
+		super(nachname, vorname, strasse, hausnummer, ort, plz, weiblich);
+		// Gehalt initialisieren
+		this.gehalt = gehalt;
+	}
 
-    /**
-     * Die Methode getGehalt() dient zur Ausgabe der Gehaltskosten. Sie wird von der
-     * Klasse Firma aufgerufen.
-     * 
-     * @param keine
-     * @return float Gehalt des Angestellten
-     */
-    public float getGehalt() {
-        return gehalt;
-    }
+	/**
+	 * Die Methode getGehalt() dient zur Ausgabe der Gehaltskosten. Sie wird von der
+	 * Klasse Firma aufgerufen.
+	 * 
+	 * @param keine
+	 * @return float Gehalt des Angestellten
+	 */
+	public float getGehalt() {
+		return gehalt;
+	}
 
-    /**
-     * Gibt die richtige Anrede zurueck.
-     * 
-     * @return die Anrede
-     */
-    @Override
-    protected String getAnrede() {
-        String anrede;
-        if (isWeiblich()) {
-            anrede = " Frau Angestellte";
-        } else {
-            anrede = " Herrn Angestellte";
-        }
-        return anrede;
-    }
+	/**
+	 * Gibt die richtige Anrede zurueck.
+	 * 
+	 * @return die Anrede
+	 */
+	@Override
+	protected String getAnrede() {
+		String anrede;
+		if (isWeiblich()) {
+			anrede = " Frau Angestellte";
+		} else {
+			anrede = " Herrn Angestellte";
+		}
+		return anrede;
+	}
+	@Override
+	public String toString() {
+		StringBuilder returnString = new StringBuilder();
+
+		returnString.append("Vorname: " + this.vorname + System.lineSeparator());
+		returnString.append("Nachname: " + this.nachname + System.lineSeparator());
+		returnString.append("Straße, Hausnummer: " + this.strasse + " " + this.hausnummer + System.lineSeparator());
+		returnString.append("PLZ Ort: " + this.plz + " " + this.ort);
+		if (isWeiblich()) {
+			returnString.append("Geschlecht: " + "weiblich" + System.lineSeparator());
+		}
+		returnString.append("Geschlecht: " + "männlich" + System.lineSeparator());
+		if (getGehalt == false )(){
+			returnString.append("Gehalt: " + "gehalt" + System.lineSeparator());
+		}
+		return returnString.toString();
+	}
 }
--- src/Kunde.java
+++ src/Kunde.java
@@ -1,88 +1,106 @@
 
 public class Kunde extends Person {
 
-    private int kundennummer;
+	private int kundennummer;
 
-    /**
-     * Parameterloser Konstruktor.
-     */
-    public Kunde() {
-        super(); // Konstruktor der Oberklasse wird aufgerufen
-    }
+	/**
+	 * Parameterloser Konstruktor.
+	 */
+	public Kunde() {
+		super(); // Konstruktor der Oberklasse wird aufgerufen
+	}
 
-    /**
-     * Mit diesem Konstruktor erfolgt die Initialisierung der Attribute durch die
-     * uebergebenen Parameter.
-     * 
-     * @param nachname   Nachname des Kunden
-     * @param vorname    Vorname des Kunden
-     * @param strasse    Strasse zur Adresse des Kunden
-     * @param hausnummer Hausnummer zur Adresse des Kunden
-     * @param ort        Ort zur Adresse des Kunden
-     * @param plz        PLZ zur Adresse des Kunden
-     * @param weiblich   Flag ob Kunde weiblich ist
-     */
-    public Kunde(String nachname, String vorname, String strasse, String hausnummer, String ort, String plz,
-            boolean weiblich) {
-        // Konstruktor der Oberklasse wird aufgerufen
-        super(nachname, vorname, strasse, hausnummer, ort, plz, weiblich);
-        this.kundennummer = 0;
-    }
+	/**
+	 * Mit diesem Konstruktor erfolgt die Initialisierung der Attribute durch die
+	 * uebergebenen Parameter.
+	 * 
+	 * @param nachname   Nachname des Kunden
+	 * @param vorname    Vorname des Kunden
+	 * @param strasse    Strasse zur Adresse des Kunden
+	 * @param hausnummer Hausnummer zur Adresse des Kunden
+	 * @param ort        Ort zur Adresse des Kunden
+	 * @param plz        PLZ zur Adresse des Kunden
+	 * @param weiblich   Flag ob Kunde weiblich ist
+	 */
+	public Kunde(String nachname, String vorname, String strasse, String hausnummer, String ort, String plz,
+			boolean weiblich) {
+		// Konstruktor der Oberklasse wird aufgerufen
+		super(nachname, vorname, strasse, hausnummer, ort, plz, weiblich);
+		this.kundennummer = 0;
+	}
 
-    /**
-     * Mit diesem Konstruktor erfolgt die Initialisierung der Attribute durch die
-     * uebergebenen Parameter inkl. setzen der Kundennummer.
-     * 
-     * @param nachname     Nachname des Kunden
-     * @param vorname      Vorname des Kunden
-     * @param strasse      Strasse zur Adresse des Kunden
-     * @param hausnummer   Hausnummer zur Adresse des Kunden
-     * @param ort          Ort zur Adresse des Kunden
-     * @param plz          PLZ zur Adresse des Kunden
-     * @param weiblich     Flag ob Kunde weiblich ist
-     * @param kundennummer Kundennummer fuer den Kunden
-     */
-    public Kunde(String nachname, String vorname, String strasse, String hausnummer, String ort, String plz,
-            boolean weiblich, int kundennummer) {
-        // Konstruktor der Oberklasse wird aufgerufen
-        super(nachname, vorname, strasse, hausnummer, ort, plz, weiblich);
-        // Kundennummer setzen
-        this.kundennummer = kundennummer;
-    }
+	/**
+	 * Mit diesem Konstruktor erfolgt die Initialisierung der Attribute durch die
+	 * uebergebenen Parameter inkl. setzen der Kundennummer.
+	 * 
+	 * @param nachname     Nachname des Kunden
+	 * @param vorname      Vorname des Kunden
+	 * @param strasse      Strasse zur Adresse des Kunden
+	 * @param hausnummer   Hausnummer zur Adresse des Kunden
+	 * @param ort          Ort zur Adresse des Kunden
+	 * @param plz          PLZ zur Adresse des Kunden
+	 * @param weiblich     Flag ob Kunde weiblich ist
+	 * @param kundennummer Kundennummer fuer den Kunden
+	 */
+	public Kunde(String nachname, String vorname, String strasse, String hausnummer, String ort, String plz,
+			boolean weiblich, int kundennummer) {
+		// Konstruktor der Oberklasse wird aufgerufen
+		super(nachname, vorname, strasse, hausnummer, ort, plz, weiblich);
+		// Kundennummer setzen
+		this.kundennummer = kundennummer;
+	}
 
-    /**
-     * Die Methode setKundenNr setzt die Kundennummer.
-     * 
-     * @param kundennummer Kunennummer der Kunden
-     */
-    public void setKundenNr(int kundennummer) {
-        this.kundennummer = kundennummer;
-    }
+	/**
+	 * Die Methode setKundenNr setzt die Kundennummer.
+	 * 
+	 * @param kundennummer Kunennummer der Kunden
+	 */
+	public void setKundenNr(int kundennummer) {
+		this.kundennummer = kundennummer;
+	}
 
-    /**
-     * Die Methode getKundenNr gibt die Kundennummer aus.
-     * 
-     * @return int Kundennummer
-     */
-    public int getKundenNr() {
-        return kundennummer;
-    }
+	/**
+	 * Die Methode getKundenNr gibt die Kundennummer aus.
+	 * 
+	 * @return int Kundennummer
+	 */
+	public int getKundenNr() {
+		return kundennummer;
+	}
 
-    /**
-     * Gibt die richtige Anrede zurueck.
-     * 
-     * @return die Anrede
-     */
-    @Override
-    protected String getAnrede() {
-        // Adressarray erzeugen
-        String anrede;
-        // Anrede nach Geschlecht festlegen
-        if (isWeiblich()) {
-            anrede = " Frau Kundin";
-        } else {
-            anrede = " Herrn Kunde";
-        }
-        return anrede;
-    }
+	/**
+	 * Gibt die richtige Anrede zurueck.
+	 * 
+	 * @return die Anrede
+	 */
+
+	@Override
+	protected String getAnrede() {
+		// Adressarray erzeugen
+		String anrede;
+		// Anrede nach Geschlecht festlegen
+		if (isWeiblich()) {
+			anrede = " Frau Kundin";
+		} else {
+			anrede = " Herrn Kunde";
+		}
+		return anrede;
+	}
+	@Override
+	public String toString() {
+		StringBuilder returnString = new StringBuilder();
+
+		returnString.append("Vorname: " + this.vorname + System.lineSeparator());
+		returnString.append("Nachname: " + this.nachname + System.lineSeparator());
+		returnString.append("Straße, Hausnummer: " + this.strasse + " " + this.hausnummer + System.lineSeparator());
+		returnString.append("PLZ Ort: " + this.plz + " " + this.ort);
+		if (isWeiblich()) {
+			returnString.append("Geschlecht: " + "weiblich" + System.lineSeparator());
+		}
+		returnString.append("Geschlecht: " + "männlich" + System.lineSeparator());
+		if (getKundennummer == false )(){
+			returnString.append("Kundennummer: " + "kundennummer" + System.lineSeparator());
+		}
+		return returnString.toString();
+	}
 }
